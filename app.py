ed7bacccf4733d53cdde8c1f79a739ff863bf752
TGEASP <abpa5533@colorado.edu>
2026-07-28 03:57:33 +0000

    Computer about to die, no good outlets at work :(

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+import os
+import psycopg2
+
 app = Flask(__name__)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Abhinavsai Parsi in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database Connection Successful"
+    except Exception as e:
+        return f"Database Connection Failed: {e}"
+    finally:
+        if conn:
+            conn.close()
